718082e744c63d8987b2bf9bc735689c5abb23be
Krzysiek <kgrygiel@gmail.com>
2017-02-24 01:03:50 +0100

    first script

---
+++ inverse_graph.py
@@ -0,0 +1,34 @@
+def inverse_graph(graph):
+    u = []
+    i = len(u)
+    for f in graph:
+         u.append(f)
+    for x in range(len(u)):
+        i = i - 1
+        if u[x][i] != 0:
+            for y in range(len(u[x])):
+                u[x][y] = 0
+        else:
+            for y in range(len(u[x])):
+                u[x][y] = 0
+                if u[x][i] == 0:
+                    u[x][i] = 1
+    return u
+
+def test():
+    g1 = [[0, 1, 1, 0],
+          [1, 0, 0, 1],
+          [1, 0, 0, 1],
+          [0, 1, 1, 0]]
+    assert inverse_graph(g1) == [[0, 0, 0, 1],
+                                 [0, 0, 1, 0],
+                                 [0, 1, 0, 0],
+                                 [1, 0, 0, 0]]
+    g2 = [[0, 1, 1, 1],
+          [1, 0, 1, 1],
+          [1, 1, 0, 1],
+          [1, 1, 1, 0]]
+    assert inverse_graph(g2) == [[0, 0, 0, 0],
+                                 [0, 0, 0, 0],
+                                 [0, 0, 0, 0],
+                                 [0, 0, 0, 0]]
